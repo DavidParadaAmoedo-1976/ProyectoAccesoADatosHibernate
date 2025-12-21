c704fb59b32b9710ad69c57dfdaa77affbb20be4
DavidParadaAmoedo1976 <davidparadadam@gmail.com>
2025-12-21 03:43:49 +0100

    primer commit

---
+++ src/main/java/marvel/Programa.java
@@ -0,0 +1,22 @@
+package marvel;
+
+import marvel.controlador.MarvelControlador;
+import marvel.modelo.util.HibernateUtil;
+import marvel.vista.MarvelVista;
+import org.hibernate.Session;
+
+public class Programa {
+
+    public static void main(String[] args) {
+
+        MarvelVista vista = new MarvelVista();
+        MarvelControlador controlador = new MarvelControlador(vista);
+
+        System.out.println("Test");
+
+        Session session = HibernateUtil.get().openSession();
+
+        session.close();
+        System.out.println("Finalizando la conexion a MySQL");
+    }
+}
---
+++ src/main/java/marvel/modelo/util/HibernateUtil.java
@@ -0,0 +1,29 @@
+package marvel.modelo.util;
+
+import java.io.File;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+
+    private static final SessionFactory SESSION_FACTORY;
+
+    static {
+        try {
+//            String hibernatePropsFilePath = ".src/main/resources/hibernate.cfg.xml"; // Ruta al fichero
+//
+//            File hibernatePropsFile = new File(hibernatePropsFilePath);
+
+            SESSION_FACTORY = new Configuration().configure().buildSessionFactory();
+
+        }catch(Throwable ex) {
+            System.err.println("Error al crear la configuración de hibernate" + ex.getMessage());
+            throw new ExceptionInInitializerError();
+        }
+    }
+
+    public static SessionFactory get() {
+        return SESSION_FACTORY;
+    }
+}
+
